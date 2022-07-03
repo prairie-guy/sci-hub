cc739a53d405e38e87012259701ea61afceda17d
prairie-guy <cdaniels@nandor.net>
2022-07-03 00:59:53 -0500

    Documentation

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 #### C. Bryan Daniels
 
-#### `doi2pdf.py` downloads and saves pdf versions of PUBMED articles from `sci-hub` servers.
+### `doi2pdf.py` downloads and saves pdf versions of PUBMED articles from `sci-hub` servers.
 
 This is a proof of concept. It should not be used to avoid copyright restrictions.
 
@@ -48,4 +48,33 @@ url_final: https://zero.sci-hub.se/2246/14a42f0c080bfe7ce33f82c060a7d572/bartley
 10.1586/eri.10.102 downloaded -> 10.1586_eri.10.102.pdf
 ```
 
+### `pmid2doi.py` takes a PubMed ID and returns (`doi,pmid`)
+
+``` python
+def pmid2doi(pmid):
+    """
+    Takes a PubMed ID `pmid` and returns a tuple (`doi`,`pmid`)
+    The signatures from within python and from the command line differ:
+    `pmid2doi(pmid) -> (doi,pmid)`
+    `./pmid2doi.py - > (doi,fname,pmid)`
+    """
+```
+
+#### Usage
+Within python:
+```python
+from pmid2doi import pmid2doi
+print(pmid2doi('30952685')) 
+```
+```
+('10.1101/pdb.prot098368', '30952685')  
+```
+
+From commnad line:
+```bash
+python doi2pdf 30952685
+```
+```
+10.1101/pdb.prot098368, 10.1101_pdb.prot098368.pdf, 30952685
+```
 
--- pmid2doi.py
+++ pmid2doi.py
@@ -3,22 +3,30 @@
 from pubmed_bibtex import bibtex_entry_from_pmid
 import re, sys
 
+# NOTE: The signatures from within python and from the command line differ:
+# pmid2doi(pmid) -> (doi,pmid)
+# ./pmid2doi.py - > (doi,fname,pmid)
+
 def pmid2doi(pmid):
+    """
+    Takes a PubMed ID `pmid` and returns a tuple (`doi`,`pmid`)
+    The signatures from within python and from the command line differ:
+    `pmid2doi(pmid) -> (doi,pmid)`
+    `./pmid2doi.py - > (doi,fname,pmid)`
+    """
     btx = bibtex_entry_from_pmid(pmid)
     doi = re.findall(r'dx\.doi\.org\/(.*?)}', btx)[0]
-    return pmid, doi
+    return doi, pmid
 
 if __name__ == '__main__':
-    # pmid = "31597913"
-    # print(pmid2doi(pmid))
     if len(sys.argv) == 2:
-        pmid, doi = pmid2doi(sys.argv[1])
+        doi, pmid = pmid2doi(sys.argv[1])
         fname = f'{doi.replace("/","_")}.pdf'
         print(f"{doi},{fname},{pmid}")
     elif len(sys.argv) == 1:
         print("doi,fname,pmid")
         for line in sys.stdin:
-            pmid, doi = pmid2doi(line.rstrip('\n'))
+            doi, pmid = pmid2doi(line.rstrip('\n'))
             fname = f'{doi.replace("/","_")}.pdf'
             print(f"{doi},{fname},{pmid}")
     else:
